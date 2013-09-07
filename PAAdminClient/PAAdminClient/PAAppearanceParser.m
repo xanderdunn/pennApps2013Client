89def5235fc385dc82e8b6dc01f70b24474cb6a2
thoughtadvances <thoughtadvances@icloud.com>
2013-09-07 17:08:19 -0400

    Fix setting the UIAppearance

--- PAAdminClient/PAAdminClient/PAAppearanceParser.m
+++ PAAdminClient/PAAdminClient/PAAppearanceParser.m
@@ -10,36 +10,35 @@
 #import <UIKit/UIKit.h>
 #import "UIFont+Parsing.h"
 #import "UIColor+Parsing.h"
+#import <objc/runtime.h>
+
+static void PARefreshViewHeirarchy(UIView *view) {
+  if (view == nil) view = [[UIApplication sharedApplication] keyWindow];
+  [view.subviews enumerateObjectsUsingBlock:^(UIView *subview, NSUInteger idx, BOOL *stop) {
+    [subview removeFromSuperview];
+    [view addSubview:subview];
+    PARefreshViewHeirarchy(subview);
+    [view setNeedsLayout];
+  }];
+  [view layoutIfNeeded];
+}
 
 @implementation PAAppearanceParser
 
 // Take an NSDictionary returned from parsing the JSON file for a UIAppearance change and apply that change
-- (BOOL)applyAppearanceWithDictionary:(NSDictionary *)dictionary {
++ (BOOL)applyAppearanceWithDictionary:(NSDictionary *)dictionary {
   NSString *className = [dictionary objectForKey:@"class"];
   Class UIKitClass = NSClassFromString(className);
   
   // Class doesn't exist
   if (!UIKitClass) return NO;
   
-  // Class doesn't conform to UIAppearance
-  if (![UIKitClass respondsToSelector:@selector(appearance)]) return NO;
+  // Are we trying to change a UI element that can be changed?
+  if (![UIKitClass conformsToProtocol:@protocol(UIAppearance)]) return NO;
   
   id appearance = [UIKitClass appearance];
-  //    NSString *capitalizedSetter = [@"set" stringByAppendingString:[propertyName capitalizedString]];
-  //    SEL selector = NSSelectorFromString(capitalizedSetter);
   
   NSString *propertyName = [dictionary objectForKey:@"property"];
-  @try {
-    [appearance valueForKey:propertyName];
-  }
-  @catch (NSException *exception) {
-    // oops
-    return NO;
-  }
-  
-  // Determine what the value is:
-  id currentValue = [appearance valueForKey:propertyName];
-  Class classOfValue = [currentValue class];
   
   id result = nil;
   
@@ -47,30 +46,51 @@
   NSString *valueString = [dictionary valueForKey:@"value"];
   NSString *classOfValueString = [dictionary valueForKey:@"type"];
   
-  result = [self parseValueString:valueString valueClassName:classOfValueString existingValueClass:(Class)classOfValue];
+  result = [self parseValueString:valueString valueClassName:classOfValueString];
   
   if (!result) return NO;
-  [appearance setValue:result forKey:propertyName];
+//  [self setAppearanceOnClass:UIKitClass value:result propertyName:propertyName];
+  
+  NSString *capitalizedLastPiece = [propertyName stringByReplacingCharactersInRange:NSMakeRange(0,1) withString:[[propertyName substringToIndex:1] uppercaseString]];
+  NSString *capitalizedSetter = [[@"set" stringByAppendingString:capitalizedLastPiece] stringByAppendingString:@":"];
+  SEL selector = NSSelectorFromString(capitalizedSetter);
+  [appearance performSelector:selector withObject:result];
+  PARefreshViewHeirarchy(nil);
   return YES;
 }
 
-- (id)parseValueString:(NSString *)value valueClassName:(NSString *)classOfValueString existingValueClass:(Class)classOfValue {
-  if ([classOfValue isSubclassOfClass:[UIColor class]]) {
++ (void)setAppearanceOnClass:(Class)class value:(id)value propertyName:(NSString *)propertyName {
+  NSUInteger count = 0;
+  objc_property_t *properties = class_copyPropertyList(class, &count);
+  for (NSUInteger i = 0; i < count; i++) {
+    NSString *name = @(property_getName(properties[i]));
+    if ([name isEqualToString:propertyName]) {
+      NSString *setter = @(property_copyAttributeValue(properties[i], "S"));
+      NSString *capitalizedLastPiece = [propertyName stringByReplacingCharactersInRange:NSMakeRange(0,1) withString:[[propertyName substringToIndex:1] uppercaseString]];
+      NSString *defaultSetter = [[@"set" stringByAppendingString:capitalizedLastPiece] stringByAppendingString:@":"];
+      SEL selector = NSSelectorFromString(setter.length ? setter : defaultSetter);
+      objc_msgSend([class appearance], selector, value);
+    }
+  }
+}
+
++ (id)parseValueString:(NSString *)value valueClassName:(NSString *)valueClassName {
+  if ([valueClassName isEqualToString:@"COLOR"]) {
     return [UIColor colorFromString:value];
-  } else if ([classOfValue isSubclassOfClass:[NSString class]]) {
+  }
+  else if ([valueClassName isEqualToString:@"STRING"]) {
     return value;
-  } else if ([classOfValue isSubclassOfClass:[NSValue class]]) {
-    if ([classOfValueString isEqualToString:@"CGSIZE"]) {
+  }
+  else if ([valueClassName isEqualToString:@"FONT"]) {
+    return [UIFont fontFromString:value];
+  }
+  else {
+    if ([valueClassName isEqualToString:@"CGSIZE"]) {
       return [NSValue valueWithCGSize:CGSizeFromString(value)];
     }
-    else if ([classOfValueString isEqualToString:@"CGRECT"]) {
+    else if ([valueClassName isEqualToString:@"CGRECT"]) {
       return [NSValue valueWithCGRect:CGRectFromString(value)];
     }
-  } else if ([classOfValue isSubclassOfClass:[NSDictionary class]]) {
-    // find a recursive substructure of how to set all the properties safely on this.
-    // #hard
-  } else if ([classOfValue isSubclassOfClass:[UIFont class]]) {
-    return [UIFont fontFromString:value];
   }
   return nil;
 }
